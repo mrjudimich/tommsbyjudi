d323e7325a51da9aae82a3034acf02b7890a576d
Judi <mrjudimich@gmail.com>
2017-04-25 21:16:45 +0300

    see

--- index.js
+++ index.js
@@ -222,7 +222,9 @@ app.post('/webhook/', function (req, res) {
                 sendProducts(sender)
                 continue
             }else if (upperCasedText.includes('CHAUSSURES')) {
-				sendTextMessage(sender,"here is an text "+event.postback.payload;);
+				if (event.postback){
+				receivedPostback(event);  
+				}
                 sendChaussures(sender)
                 continue
             }else if (upperCasedText.includes('PARFUMS')) {
@@ -301,6 +303,23 @@ var FACEBOOK_ACCESS_TOKEN = "EAAI7ccGDI6ABADJKEC1400oTDlO8aslbvaBTzZAjwobZBoYn35
  * @param {Object} messageData - The contents of the message in Facebook-compliant JSON
  * @return nothing
  */
+ function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, "Received postback for user "+senderID+" and page "+recipientID+" with payload "+payload+" at "+timeOfPostback);
+}
+
 function sendMessage(sender, messageData) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
